56837a55cb2c91631e01d06af65ce083f6b823f8
guilherme.lacerda <guilherme.lacerda@neuralmed.ai>
2024-05-20 20:34:24 -0300

    chore(aulas): tres primeiras aulas do curso

---
+++ aula2.py
@@ -0,0 +1,14 @@
+valorhora=4
+dias=30
+horastrabalho=8
+
+vencimentomensal=valorhora*horastrabalho*dias
+nome="Guilherme"
+
+print("")
+print(valorhora)
+print(dias)
+print(horastrabalho)
+print(vencimentomensal)
+print(nome)
+print(float(vencimentomensal))
